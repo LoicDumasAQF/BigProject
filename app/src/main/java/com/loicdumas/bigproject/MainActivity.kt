b7cd4c46802b988e743390059f59f90e8ceb1065
Loic <loic.dumas@rakuten.com>
2018-09-12 11:39:32 +0200

    add set project in same folder

--- app/src/main/java/com/loicdumas/bigproject/MainActivity.kt
+++ app/src/main/java/com/loicdumas/bigproject/MainActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.loicdumas.calculator.AnotherCalculator
 import com.loicdumas.calculator.Calculator
+import com.loicdumas.readercalculatorlib.ReaderCalculator
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -19,10 +20,11 @@ class MainActivity : AppCompatActivity() {
         val c = 15
 
 
-        tv_main_activity.text = "Somme de $a + $b + $c = ${Calculator.addition(a, b, c)}"
+        tv_main_activity.text = "BIG Project !"
 
+        tv_main_activity.text = "${tv_main_activity.text.toString()} \n\n Somme de $a + $b + $c = ${Calculator.addition(a, b, c)}"
+        tv_main_activity.text = "${tv_main_activity.text.toString()} \n\n Somme de $a + $b = ${AnotherCalculator.addition(a, b)}"
 
-
-        tv_main_activity.text = "${tv_main_activity.text.toString()} \n Somme de $a + $b = ${AnotherCalculator.addition(a, b)}"
+        tv_main_activity.text = "${tv_main_activity.text.toString()} \n\n Somme de $a + $b + $c = ${ReaderCalculator.readerAddition(a, b, c)}"
     }
 }
